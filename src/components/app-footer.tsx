b57fb65326e235c05557e2b7c4c73d3e78bdc2a1
اتحاد طلاب تحيا مصر <tahyamisr.online@gmail.com>
2025-09-18 19:21:22 +0000

    اجعل الحجم Bold (_for element p_)

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -2,7 +2,7 @@ export default function AppFooter() {
   return (
     <footer className="bg-white mt-auto border-t">
         <div className="container mx-auto px-6 py-4 text-center text-gray-600">
-            <p>&copy; 2025 لجنة التنظيم والمراسم - اتحاد طلاب تحيا مصر. جميع الحقوق محفوظة.</p>
+            <p className="font-bold">&copy; 2025 لجنة التنظيم والمراسم - اتحاد طلاب تحيا مصر. جميع الحقوق محفوظة.</p>
         </div>
     </footer>
   );
